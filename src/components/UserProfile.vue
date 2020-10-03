a9a5908ed7a6d260d6f62a283c9e6e8bb93acc87
Sebastian Strus <sebastian.strus1@gmail.com>
2020-10-04 00:59:40 +0200

    Play around with Vue
    Created some user and posts.

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+    </div>
+    <div class="user-profile__tweets-wrapper">
+      <TweetItem
+        v-for="tweet in user.tweets"
+        :key="tweet.id"
+        :username="user.username"
+        :tweet="tweet"
+        @favourite="toggleFavourite"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TweetItem from "./TweetItem";
+
+export default {
+  name: "UserProfile",
+  components: { TweetItem },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_SebastianStrus",
+        firstName: "Sebastian",
+        lastName: "Strus",
+        email: "sebastian.strus1@gmail.com",
+        isAdmin: true,
+        tweets: [
+          { id: 1, content: "TwitterApp is cool!" },
+          { id: 2, content: "Don't forget to subscribe!" },
+        ],
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerAccount, oldFollowerAccount) {
+      if (oldFollowerAccount < newFollowerAccount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+    toggleFavourite(id) {
+      console.log(`Favourited tweet #${id}`);
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 50px;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+h1 {
+  margin: 0;
+}
+
+.user-profile__tweets-wrapper {
+  display: grid;
+  grid-gap: 10px;
+}
+</style>
