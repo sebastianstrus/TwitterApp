c924247587ee5f0faed2a85bcd3ec0308c7d385b
Sebastian Strus <sebastian.strus1@gmail.com>
2020-10-28 23:58:32 +0100

    Replaced ec2 instance id

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://ec2-13-49-221-202.eu-north-1.compute.amazonaws.com:8080'
+axios.defaults.baseURL = 'http://ec2-13-48-6-58.eu-north-1.compute.amazonaws.com:8080'
 
 createApp(App).use(store).use(router).mount('#app')
